da0a42d6d93b777f52e6cdb42cd03af258aff060
viveksemwal <Viveksemwal1997@gmail.com>
2020-11-30 18:05:50 +0530

    Adding Basic Structure for Dashboard

---
+++ ppmtoolreact/src/component/Header/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <h1>Navbar</h1>
+        )
+    }
+}
+
+export default Navbar;
