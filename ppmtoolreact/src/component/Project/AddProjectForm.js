178deb7a64d45f2d121f6a65695eeacf6bed6eb9
viveksemwal <Viveksemwal1997@gmail.com>
2020-12-02 23:24:59 +0530

    created states for storing and sending form date and tested next starting redux

--- ppmtoolreact/src/component/Project/AddProjectForm.js
+++ ppmtoolreact/src/component/Project/AddProjectForm.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react'
 
  class AddProjectForm extends Component {
+     constructor(){
+         super()
+         this.state={
+            projectName:"",
+            projectIdentifier:"",
+            description:"",
+            startDate:"",
+            endDate:""
+         }
+     }
+
+     onChange(e){
+         this.setState({[e.target.name]:e.target.value})
+     }
+
+     onSubmit(e){
+         e.preventDefault();
+         const newProject ={
+            projectName:this.state.projectName,
+            projectIdentifier:this.state.projectIdentifier,
+            description:this.state.description,
+            startDate:this.state.startDate,
+            endDate:this.state.endDate
+         }
+
+         
+
+     }
+     
     render() {
         return (
-            <div>
-                <h1>Add Project form</h1>
+            <div className="project">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 m-auto">
+                    <h5 className="display-4 text-center">Create </h5>
+                    <hr />
+                    <form onSubmit={this.onSubmit.bind(this)}>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange.bind(this)}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name="projectIdentifier" value={this.state.projectIdentifier} onChange={this.onChange.bind(this)}/>
+                        </div>
+                        
+                        <div className="form-group">
+                            <textarea className="form-control form-control-lg" placeholder="Project Description" name="description" value={this.state.description} onChange={this.onChange.bind(this)}></textarea>
+                        </div>
+                        <h6>Start Date</h6>
+                        <div className="form-group">
+                            <input type="date" className="form-control form-control-lg" name="startDate" value={this.state.startDate} onChange={this.onChange.bind(this)}/>
+                        </div>
+                        <h6>Estimated End Date</h6>
+                        <div className="form-group">
+                            <input type="date" className="form-control form-control-lg" name="endDate" value={this.state.endDate}  onChange={this.onChange.bind(this)}/>
+                        </div>
+
+                        <input type="submit" className="btn btn-primary btn-block mt-4" />
+                    </form>
+                </div>
             </div>
+        </div>
+    </div>
         )
     };
 };
